6f2368f46a30de68546f656d27bf77e838bd4c54
michaellapoint <ml8043@nyu.edu>
2022-02-13 10:53:34 -0500

    updated headers

--- solution.py
+++ solution.py
@@ -17,12 +17,13 @@ def webServer(port=13331):
                 filename = message.split()[1]
                 f = open(filename[1:])
                 outputdata = f.read()
-                #print(outputdata)
+                # print(outputdata)
                 # Send one HTTP header line into socket.
                 #print("Sending header")
                 connectionSocket.send("HTTP/1.1 200 OK\r\n".encode())
+                connectionSocket.send("Content_Type: text / html; charset = UTF - 8\r\n".encode())
                 # Send the content of the requested file to the client
-                #print("Sending file")
+                # print("Sending file")
                 for i in range(0, len(outputdata)):
                     connectionSocket.send(outputdata[i].encode())
                 connectionSocket.send("\r\n".encode())
@@ -30,12 +31,12 @@ def webServer(port=13331):
             except IOError:
                 # Send response message for file not found (404)
                 #print("File not found")
-                connectionSocket.send("HTTP/1.1 404 File not found\r\n".encode())
+                connectionSocket.send("HTTP/1.1 404 Not Found\r\n".encode())
                 connectionSocket.close()
         except (ConnectionResetError, BrokenPipeError):
             #print("connection or pipe error")
             pass
-        serverSocket.close()
+            serverSocket.close()
         sys.exit()  # Terminate the program after sending the corresponding data
 
 if __name__ == "__main__":
